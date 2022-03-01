39c30c9914a4fd806ba88d35e998b678e1c2ad6a
Motti789 <mchillywilly@gmail.com>
2022-03-01 15:01:56 -0500

    commit tree 9eb51dd859067b712b50a3e8a5306c1862f0882f

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeStr) {
+
+  const hour = parseInt(timeStr);
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+
+}
+
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg;
+
+}
